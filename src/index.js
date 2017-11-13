33314355f65c4630edeb5fa8a074bbfe50650535
Yauheni <mickevich.ea@gmail.com>
2017-11-14 02:35:07 +0300

    add seatch books

--- src/index.js
+++ src/index.js
@@ -32,6 +32,19 @@ app.post('/all-books', function(req, res){
    })
  })
 
+ app.post('/search-books', function(req, res){
+  var re = new RegExp(req.body.message);
+  ///re/i
+  Book.find({name: re}, function(err, books) {
+      if(err){
+        console.log(err);
+      } else {
+        // console.log(books);
+        res.status(200).json({allBooks: books});
+      }
+    })
+  })
+
 app.get('/', function(req, res){
   res.send('Hello in browser')
 })
